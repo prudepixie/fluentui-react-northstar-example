518e4923fb969b6b84f100e9b82847156ff8631d
Wendy <wendy@Wendys-MacBook-Pro-Microsoft.local>
2022-02-04 19:37:15 -0800

    button

---
+++ src/Button.tsx
@@ -0,0 +1,3 @@
+import { Button } from '@fluentui/react-northstar'
+
+export default () => <Button content="Button" primary/>
--- src/index.tsx
+++ src/index.tsx
@@ -1,21 +1,11 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import { App } from './App';
-import { mergeStyles } from '@fluentui/react';
-import reportWebVitals from './reportWebVitals';
+import ReactDOM from 'react-dom'
+import { Provider, teamsTheme } from '@fluentui/react-northstar'
 
-// Inject some global styles
-mergeStyles({
-  ':global(body,html,#root)': {
-    margin: 0,
-    padding: 0,
-    height: '100vh',
-  },
-});
+import {App} from './App'
 
-ReactDOM.render(<App />, document.getElementById('root'));
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+ReactDOM.render(
+  <Provider theme={teamsTheme}>
+    <App />
+  </Provider>,
+  document.getElementById('root'),
+)
