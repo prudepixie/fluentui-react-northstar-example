10e45d338341fb3d8762cf6c1e26cbec4f2aa067
Wendy <wendy@Wendys-MacBook-Pro-Microsoft.local>
2022-02-10 10:46:56 -0800

    delete button code

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,8 @@
 import React from 'react';
 import './App.css';
-import Button from "./Button"
 
 export const App: React.FunctionComponent = () => {
   return (
-      <Button></Button>
+      <div></div>
   );
 };
--- src/Button.tsx
+++
@@ -1,3 +0,0 @@
-import { Button } from '@fluentui/react-northstar'
-
-export default () => <Button content="Button" primary/>
