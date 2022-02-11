49cf6751410c005cc182b321b5916b5a2c146fad
Wendy <wendy@Wendys-MacBook-Pro-Microsoft.local>
2022-02-11 09:47:53 -0800

    take out slots

--- src/Card.tsx
+++ src/Card.tsx
@@ -2,18 +2,14 @@ import { Card, Flex, Text } from '@fluentui/react-northstar'
 
 const CardExampleHeader = () => (
   <Card aria-roledescription="card avatar">
-    <Card.Header fitted>
-      <Flex gap="gap.small">
+    <Flex gap="gap.small">
         <img src={`${process.env.PUBLIC_URL}/matt.jpg`} alt="matt avatar"/>
-        <Flex column>
-          <Text content="Title goes here" weight="bold" />
-          <Text content="Secondary line" size="small" />
-        </Flex>
-      </Flex>
-    </Card.Header>
-    <Card.Body fitted>
-        <Text content="Citizens of distant epochs muse about at the edge of forever hearts of the..." />
-    </Card.Body>
+    <Flex column>
+        <Text content="Title goes here" weight="bold" />
+        <Text content="Secondary line" size="small" />
+    </Flex>
+    </Flex>
+    <Text content="Citizens of distant epochs muse about at the edge of forever hearts of the..." />
   </Card>
 )
 
